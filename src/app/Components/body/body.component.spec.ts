c79aae91697489aad10cc7c181de0eedccbc05fc
Akhil Prem <akhilpchand@gmail.com>
2024-02-03 17:22:01 +0530

    Added test for body component

--- src/app/Components/body/body.component.spec.ts
+++ src/app/Components/body/body.component.spec.ts
@@ -2,13 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BodyComponent } from './body.component';
 
-describe('BodyComponent', () => {
+import { AppModule } from '../../app.module';
+
+fdescribe('BodyComponent', () => {
   let component: BodyComponent;
   let fixture: ComponentFixture<BodyComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BodyComponent]
+      imports: [AppModule]
     })
     .compileComponents();
     
@@ -19,5 +21,30 @@ describe('BodyComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it ("should hide book details if it is currently displaying",()=>{
+
+    component.bookDetails="This is a test";
+
+    component.getBookDetails(2);
+
+    expect(component.bookDetails).toEqual("");
+
+    
+    
+
+
+  });
+  it("should display book details if it is not showing anything",()=>{
+
+    component.bookDetails="";
+
+    component.getBookDetails(1);
+
+    expect(component.bookDetails).toEqual(component.bookArray[1].desc);
+
+    
+
   });
 });
