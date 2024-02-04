e088b23ed54a3b5c4e4be31ae865013d65cd0a3e
Akhil Prem <akhilpchand@gmail.com>
2024-02-04 15:36:59 +0530

    Added test for number directive

--- src/app/number.directive.spec.ts
+++ src/app/number.directive.spec.ts
@@ -1,8 +1,41 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { NumberDirective } from './number.directive';
+import { HeaderComponent } from './Components/header/header.component';
 
-describe('NumberDirective', () => {
+
+fdescribe('NumberOnlyDirective', () => {
+  let fixture: ComponentFixture<HeaderComponent>;
+  let inputelement: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [NumberDirective, HeaderComponent],
+    });
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    inputelement = fixture.debugElement.query(By.css('input'));
+  });
+
   it('should create an instance', () => {
     const directive = new NumberDirective();
     expect(directive).toBeTruthy();
   });
+
+  it('should allow numeric keys and Backspace', () => {
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Backspace'].forEach((key) => {
+      const event = new KeyboardEvent('keydown', { key });
+      inputelement.triggerEventHandler('keydown', event);
+      expect(event.defaultPrevented).toBe(false);
+    });
+ });
+
+  it('should prevent non-numeric input', () => {
+    inputelement.triggerEventHandler('keydown', { key: 'A' });
+    fixture.detectChanges();
+    expect(inputelement.nativeElement.value).toBe('');
+  });
+
+  
 });
